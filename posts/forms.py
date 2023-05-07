ad58448a7168c91ecce1a281523dd741d98bec01
Ellie <yokomone@yahoo.com>
2023-05-08 01:04:06 +0300

    Update forms.py

--- posts/forms.py
+++ posts/forms.py
@@ -5,6 +5,5 @@ from posts.models import Post
 class PostForm(forms.ModelForm):
     class Meta:
         model = Post
-        fields = ['name', 'age', 'role', 'country', 'languages', 'team_experience', 'tournament_experience',
-                  'faceit_esportal_esea', 'discord', 'steam']
-
+        fields = ['name', 'age', 'role', 'country', 'languages', 'discord', 'steam', 'team_experience',
+                  'tournament_experience', 'faceit_esportal_esea']
