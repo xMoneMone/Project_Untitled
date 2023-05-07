e05eec59ff4775d45a9c66056b9a6ee438505496
Ellie <yokomone@yahoo.com>
2023-05-08 00:17:17 +0300

    created post model

--- posts/models.py
+++ posts/models.py
@@ -1,3 +1,17 @@
 from django.db import models
 
-# Create your models here.
+
+class Post(models.Model):
+    role_choices = (('IGL', 'IGL'), ('Lurk', 'Lurk'), ('Support', 'Support'), ('Entry', 'Entry'),
+                    ('AWP', 'AWP'), ('Secondary AWP', 'Secondary AWP'))
+
+    name = models.CharField(max_length=12)
+    age = models.IntegerField()
+    role = models.CharField(max_length=20, choices=role_choices)
+    country = models.CharField(max_length=60)
+    languages = models.CharField(max_length=200)
+    team_experience = models.TextField(max_length=600)
+    tournament_experience = models.TextField(max_length=600)
+    faceit_esportal_esea = models.TextField(max_length=600)
+    discord = models.CharField(max_length=60)
+    steam = models.CharField(max_length=60)
