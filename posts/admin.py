8cb410003c57250e9332c1e4a88cd1457e6263f4
Ellie <yokomone@yahoo.com>
2023-05-10 03:01:42 +0300

    better admin representation of posts

--- posts/admin.py
+++ posts/admin.py
@@ -1,4 +1,6 @@
 from django.contrib import admin
 from posts.models import Post
 
-admin.site.register(Post)
+@admin.register(Post)
+class PostAdmin(admin.ModelAdmin):
+    list_display = ('id', 'name', 'age', 'languages', 'role')
